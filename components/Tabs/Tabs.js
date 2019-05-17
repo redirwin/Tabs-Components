962ddf3dabedfe6e9bee5bb6577573d6b6d509aa
Dave <redirwin@gmail.com>
2019-05-16 17:13:32 -0700

    Minor update to tabs assignemnt: removed Array.from since a node list was used.

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -26,7 +26,7 @@ class TabLink {
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     // Array.from(links).forEach();
-    Array.from(links).forEach(link => { // Why do we need Array.from here?
+    links.forEach(link => {
       link.classList.remove('tabs-link-selected');
     })
 
